40dcef19cf169d6ed6b1d9b7cf1f99ce7093df7e
Messbah.A <messbah.a@gmail.com>
2025-03-05 08:10:44 -0800

    payment added

---
+++ view/order_view.py
@@ -0,0 +1,135 @@
+#alifarazi
+from PyQt5.QtWidgets import QWidget
+
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'designerGgSyZo.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QHeaderView, QKeySequenceEdit,
+    QLabel, QPushButton, QSizePolicy, QTableWidget,
+    QTableWidgetItem, QWidget)
+
+# todo
+class Ui_Dialog(QWidget):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(654, 380)
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(550, 330, 91, 31))
+        self.tableWidget = QTableWidget(Dialog)
+        if (self.tableWidget.columnCount() < 5):
+            self.tableWidget.setColumnCount(5)
+        font = QFont()
+        font.setPointSize(9)
+        __qtablewidgetitem = QTableWidgetItem()
+        __qtablewidgetitem.setFont(font);
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, __qtablewidgetitem4)
+        if (self.tableWidget.rowCount() < 3):
+            self.tableWidget.setRowCount(3)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(0, __qtablewidgetitem5)
+        __qtablewidgetitem6 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(1, __qtablewidgetitem6)
+        __qtablewidgetitem7 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(2, __qtablewidgetitem7)
+        self.tableWidget.setObjectName(u"tableWidget")
+        self.tableWidget.setGeometry(QRect(0, 0, 571, 192))
+        self.keySequenceEdit = QKeySequenceEdit(Dialog)
+        self.keySequenceEdit.setObjectName(u"keySequenceEdit")
+        self.keySequenceEdit.setGeometry(QRect(90, 210, 113, 21))
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 210, 81, 16))
+        self.label_2 = QLabel(Dialog)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 250, 81, 16))
+        self.label_3 = QLabel(Dialog)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(220, 210, 81, 16))
+        self.label_4 = QLabel(Dialog)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(220, 250, 81, 16))
+        self.label_5 = QLabel(Dialog)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(10, 290, 81, 16))
+        self.label_6 = QLabel(Dialog)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(10, 330, 81, 16))
+        self.keySequenceEdit_2 = QKeySequenceEdit(Dialog)
+        self.keySequenceEdit_2.setObjectName(u"keySequenceEdit_2")
+        self.keySequenceEdit_2.setGeometry(QRect(90, 250, 113, 21))
+        self.keySequenceEdit_3 = QKeySequenceEdit(Dialog)
+        self.keySequenceEdit_3.setObjectName(u"keySequenceEdit_3")
+        self.keySequenceEdit_3.setGeometry(QRect(300, 210, 113, 21))
+        self.keySequenceEdit_4 = QKeySequenceEdit(Dialog)
+        self.keySequenceEdit_4.setObjectName(u"keySequenceEdit_4")
+        self.keySequenceEdit_4.setGeometry(QRect(300, 250, 113, 21))
+        self.keySequenceEdit_5 = QKeySequenceEdit(Dialog)
+        self.keySequenceEdit_5.setObjectName(u"keySequenceEdit_5")
+        self.keySequenceEdit_5.setGeometry(QRect(90, 290, 113, 21))
+        self.keySequenceEdit_6 = QKeySequenceEdit(Dialog)
+        self.keySequenceEdit_6.setObjectName(u"keySequenceEdit_6")
+        self.keySequenceEdit_6.setGeometry(QRect(90, 330, 113, 21))
+        self.pushButton_2 = QPushButton(Dialog)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(450, 330, 91, 31))
+        self.pushButton_3 = QPushButton(Dialog)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(350, 330, 91, 31))
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"save", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("Dialog", u"order id", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Dialog", u"order", None));
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("Dialog", u"product", None));
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("Dialog", u"quantity", None));
+        ___qtablewidgetitem4 = self.tableWidget.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("Dialog", u"price", None));
+        ___qtablewidgetitem5 = self.tableWidget.verticalHeaderItem(0)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("Dialog", u"product 1", None));
+        ___qtablewidgetitem6 = self.tableWidget.verticalHeaderItem(1)
+        ___qtablewidgetitem6.setText(QCoreApplication.translate("Dialog", u"product 2", None));
+        ___qtablewidgetitem7 = self.tableWidget.verticalHeaderItem(2)
+        ___qtablewidgetitem7.setText(QCoreApplication.translate("Dialog", u"product 3", None));
+        self.label.setText(QCoreApplication.translate("Dialog", u"product name", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"order id", None))
+        self.label_3.setText(QCoreApplication.translate("Dialog", u"order", None))
+        self.label_4.setText(QCoreApplication.translate("Dialog", u"product", None))
+        self.label_5.setText(QCoreApplication.translate("Dialog", u"quantity", None))
+        self.label_6.setText(QCoreApplication.translate("Dialog", u"price", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Dialog", u"back", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Dialog", u"cancel", None))
+        self.show()
